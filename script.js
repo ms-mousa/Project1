7c3ef6f52794fd44842091b3903397cd84840f45
Ms-Mousa <Contact@mahmoud.blog>
2019-10-11 16:22:15 +1100

    finished first section intro

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-function myFunction() {
-    alert("hello");
-}
+$(window).scroll(function() {
+    if ($(window).scrollTop() >= 400) {
+        $('.scrll--shadow').css({
+            "animation": ""
+        });
+    } else {
+        $('.scrll--shadow').css({
+            "animation": "bounce 2s infinite"
+        });
+    }
+
+})
+
+$(document).ready(function() {
+    $('.scrll--shadow').css({ "animation": "bounce 2s infinite" });
+});
